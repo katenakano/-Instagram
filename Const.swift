4a7dcb84179c1619aad2a5760d3434ca81d8c793
Hiroko Nakano <hirokonakano@hirokonomacbook-pro.local>
2018-11-25 22:36:07 +0900

    途中経過です

---
+++ Const.swift
@@ -0,0 +1,15 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Hiroko Nakano on 2018/11/25.
+//  Copyright © 2018 katenakano. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
